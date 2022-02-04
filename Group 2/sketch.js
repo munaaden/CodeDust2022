dcab4dd549fe25d1898baee82a33d47b9edd7a00
munaaden <90456692+munaaden@users.noreply.github.com>
2022-02-04 16:19:45 +0300

    Add files via upload

---
+++ Group 2/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(700, 400);
+}
+
+function draw() {
+  background(0, 126, 255);
+  ellipse(190, 150, 150, 200);
+  fill(200);
+rect(256, 205, 150, 150); 
+  fill(200,00,600);
+}
