9d13312d0fbf3d59f98aa0f1492d4c7bf72106e0
munaaden <90456692+munaaden@users.noreply.github.com>
2022-02-04 16:21:10 +0300

    Add files via upload

---
+++ Print_Name/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+
+function draw() {
+  background(0,0,220);
+}
+function person(){
+  var first_name= " MUNA "
+  
+  var middle_name= " ADEN "
+  var last_name= "ISSAK"
+  console.log(first_name + middle_name + last_name)
+}
+person();
