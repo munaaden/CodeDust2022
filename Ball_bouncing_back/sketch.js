941c8ebabcd688695dfdcf702693dfdb7b58ef95
munaaden <90456692+munaaden@users.noreply.github.com>
2022-02-04 16:06:37 +0300

    Add files via upload

---
+++ Ball_bouncing_back/sketch.js
@@ -0,0 +1,21 @@
+var x = 0;
+var speed = 3;
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0,23);
+  stroke(255);
+  strokeWeight(4);
+  fill(0,255,0);
+  ellipse(x, 200, 100, 100);
+  
+  if (x > width){
+    speed = -3;
+  }
+  
+  
+  
+  x = x + speed;
+}
