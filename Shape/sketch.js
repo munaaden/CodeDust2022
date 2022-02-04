9787c813cede67b5eac153471a987007d2afc63a
munaaden <90456692+munaaden@users.noreply.github.com>
2022-02-04 16:21:56 +0300

    Add files via upload

---
+++ Shape/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(700, 500);
+}
+
+function draw() {
+  background('#0f0');
+  rect(200,200,150,100)
+  circle(270,150,100)
+  rect(230,299,35,150)
+  rect(290,299,35,150)
+}
